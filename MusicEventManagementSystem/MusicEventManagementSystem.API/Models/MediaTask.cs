eae16c77dd45abb6795d8593fd5f9db01a6b8f70
Marina <marinakhan1801@gmail.com>
2025-09-06 01:38:34 +0200

    SCRUM-88 CRUD Operations for tasks not done

---
+++ MusicEventManagementSystem/MusicEventManagementSystem.API/Models/MediaTask.cs
@@ -0,0 +1,11 @@
+﻿namespace MusicEventManagementSystem.API.Models
+{
+    public class MediaTask
+    {
+        public int IdTask { get; set; }
+        public string? TaskName { get; set; }
+        public int Order { get; set; }
+        public string? TaskStatus { get; set; }
+        public int IdWorkflow { get; set; }
+    }
+}
---
+++ MusicEventManagementSystem/MusicEventManagementSystem.API/Services/IService/IMediaTaskService.cs
@@ -0,0 +1,12 @@
+﻿using MusicEventManagementSystem.API.Models;
+namespace MusicEventManagementSystem.API.Services.IService
+{
+    public interface IMediaTaskService
+    {
+        Task<IEnumerable<MediaTask>> GetAllTasksAsync();
+        Task<Task?> GetTaskByIdAsync(int id);
+        Task<Task> CreateTaskAsync(Task task);
+        Task<Task?> UpdateTaskAsync(int id, Task task);
+        Task<bool> DeleteTaskAsync(int id);
+    }
+}
